10bdbc2a448d8fce5f2601b4466862e72aaf2fe3
David Hong <davidhong.code@gmail.com>
2012-01-19 14:31:03 +1100

    New Glossary rails app created. It is configured to use mongodb (via mongo_mapper).

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlossyRails::Application.config.secret_token = 'd922396eb273c573d9af798d83fadf37b9b5896e6d48cb1c7b55a914198ee5b839f1ecc4cb52d5f19ed7a33af60987eb7d639a0c00b39381facec91e3f624dbe'
