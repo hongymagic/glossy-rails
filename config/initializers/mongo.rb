65f6fd55e9578831a36541df077c69ea1f7eb75d
David Hong <davidhong.code@gmail.com>
2012-01-19 15:05:00 +1100

    Adding MongoHQ configuration for product environment

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,4 +1,4 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+# MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
 MongoMapper.database = "#myapp-#{Rails.env}"
 
 if defined?(PhusionPassenger)
