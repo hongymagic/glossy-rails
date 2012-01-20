f04ba710d2aaa95049e0cfca9545541a51c4f08d
David Hong <davidhong.code@gmail.com>
2012-01-20 22:52:23 +1100

    Removing uneccessay mongo_mapper settings

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,6 +1,6 @@
-MongoMapper.database = "glossy-#{Rails.env}"
 MongoMapper.config = { 
-  Rails.env => { 'uri' => ENV['MONGOHQ_URL'] || 
-                          'mongodb://localhost/sushi' } }
-
+  Rails.env => { 
+    'uri' => ENV['MONGOHQ_URL'] || 'mongodb://localhost/sushi' 
+  } 
+}
 MongoMapper.connect(Rails.env)
