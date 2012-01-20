6605bee7ba1bb638979415a3094a300336a7c32c
David Hong <davidhong.code@gmail.com>
2012-01-20 14:25:35 +1100

    Adding simple model validation on Definition

--- app/models/definition.rb
+++ app/models/definition.rb
@@ -4,4 +4,8 @@ class Definition
   key :term, String
   key :definition, String
 
+  # Validations
+  validates_presence_of :term, :definition
+  validates :term, :uniqueness => true, :format => { :with => /[A-Za-z0-9]+/ }
+  validates :definition, :length => { :maximum => 256 }
 end
